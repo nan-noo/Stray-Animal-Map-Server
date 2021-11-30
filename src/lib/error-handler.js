5865bd1e935094fc83c5ee4040118aa288e99067
nan-noo <uni613@naver.com>
2021-11-30 23:33:30 +0900

    feat: create error-handler.js
    for easier error handling

---
+++ src/lib/error-handler.js
@@ -0,0 +1,14 @@
+const errorHandler = (err, req, res, next) => {
+    switch (err.message) {
+        case 'BAD_REQUEST':
+            return res.json({success: false, message: "잘못된 요청 형식입니다."});
+        case 'UNAUTHORIZED':
+            return res.json({success: false, message: "잘못된 비밀번호입니다."});
+        case 'NOT_FOUND':
+            return res.json({success: false, message: "일치하는 e-mail이 없습니다."});
+        default:
+            return res.json({success: false, err});
+    }
+};
+
+module.exports = { errorHandler };
