2231942d4c125ac224e5f74951b560c0491bcfd1
nan-noo <uni613@naver.com>
2021-12-01 15:10:45 +0900

    feat: fix error-handler.js
    Add console.error

--- src/lib/error-handler.js
+++ src/lib/error-handler.js
@@ -9,6 +9,7 @@ const errorHandler = (err, req, res, next) => {
         case NOT_FOUND:
             return res.json({success: false, message: "일치하는 e-mail이 없습니다."});
         default:
+            if (process.env.MODE !== 'prod') console.error('\x1b[31m%s\x1b[0m', err);
             return res.json({success: false, err});
     }
 };
