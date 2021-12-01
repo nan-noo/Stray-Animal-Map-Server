feca01efeaa46d871e74984f1fb443ab0b75575b
nan-noo <uni613@naver.com>
2021-12-01 15:21:15 +0900

    refactor: error-handler.js

--- src/lib/error-handler.js
+++ src/lib/error-handler.js
@@ -10,7 +10,7 @@ const errorHandler = (err, req, res, next) => {
             return res.json({success: false, message: "일치하는 e-mail이 없습니다."});
         default:
             if (process.env.MODE !== 'prod') console.error('\x1b[31m%s\x1b[0m', err);
-            return res.json({success: false, err});
+            return res.json({success: false, message: err.message});
     }
 };
 
